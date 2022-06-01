14d908f55d77b5d90a187c12c97e84c1551a036d
martandsingh <martandsingh>
2022-06-01 10:47:36 +0000

    SETUP INFORMATION

--- SQL Refresher/SR000-Introduction.py
+++ SQL Refresher/SR000-Introduction.py
@@ -15,12 +15,18 @@
 # MAGIC 1. UNION, INTERSECT, EXCEPT keywords
 # MAGIC 
 # MAGIC you can download all the notebook from our 
+# MAGIC 
 # MAGIC github repo: https://github.com/martandsingh/ApacheSpark
+# MAGIC 
 # MAGIC facebook: https://www.facebook.com/codemakerz
+# MAGIC 
 # MAGIC email: martandsays@gmail.com
+# MAGIC 
+# MAGIC ### SETUP folder
+# MAGIC you will see initial_setup & clean_up notebooks called in every notebooks. It is mandatory to run both the scripts in defined order. initial script will create all the mandatory tables & database for the demo. After you finish your notebook, execute clean up notebook, it will clean all the db objects.
 # MAGIC 
 # MAGIC ![SQL](https://raw.githubusercontent.com/martandsingh/images/master/sql.png)
 
 # COMMAND ----------
 
-
+# MAGIC %
